8335fdf11f917128b8336bb93bd9d4da8d047144
Caitlin Riggs <caitriggs@gmail.com>
2014-03-29 20:26:13 -0400

    Add media query for mobile sized screen, crop martsa_main.png

--- templates/header.php
+++ templates/header.php
@@ -1,23 +1,25 @@
 <!DOCTYPE html>
 <head>
-	<meta charset="utf-8">
-	<title>Martsa on Elm - Tibetan Cuisine</title>
+    <meta charset="utf-8">
+    <title>Martsa on Elm - Tibetan Cuisine</title>
     <link rel="shortcut icon" type="image/png" href="img/favicon.ico"/>
     <link rel="stylesheet" href="css/normalize.css">
-	<link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/main.css">
+    <!-- For responsive design on mobile -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
 <body>
     <div id="header-container">
-    	<nav>
+        <nav>
             <ul>
-                <li><a href="index.html">Home</a></li>
-                <li><a href="#">Menu</a></li>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="menu.php">Menu</a></li>
             </ul>
-            <a href="index.html">
+            <a href="index.php">
                 <img id="martsa-main" src="img/martsa_main.png"/>
             </a>
             <ul>
-                <li><a href="#">Photos</a></li>
+                <li><a href="#">About</a></li>
                 <li><a href="#">Contact</a></li>
             </ul>
         </nav>
