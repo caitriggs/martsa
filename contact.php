e7b2bd77a03d9c6327fc2520ee6b5975cde162d9
Caitlin Riggs <caitriggs@gmail.com>
2014-03-30 20:08:13 -0400

    Add marker to map on Contact page

--- contact.php
+++ contact.php
@@ -5,13 +5,19 @@
 	<script src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
     <script>
       function initialize() {
+      	var myLatlng = new google.maps.LatLng(42.3949, -71.1215);
         var map_canvas = document.getElementById('map-canvas');
         var map_options = {
-          center: new google.maps.LatLng(42.3949, -71.1215),
+          center: myLatlng,
           zoom: 17,
           mapTypeId: google.maps.MapTypeId.ROADMAP
         }
         var map = new google.maps.Map(map_canvas, map_options)
+        var marker = new google.maps.Marker({
+	      position: myLatlng,
+	      map: map,
+	      title: 'Hello World!'
+	  	});
       }
       google.maps.event.addDomListener(window, 'load', initialize);
     </script>	
